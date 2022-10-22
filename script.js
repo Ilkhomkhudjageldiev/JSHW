e27465e68cca30dabc3161028c950b3c1963f4fb
ILKHOM <ilhom.hudjageldiyev@gmail.com>
2022-10-22 21:30:43 +0500

    Uy ishi1

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+var userName = prompt('Hurmatli foydalanuvchi, Ismingizni kiriting')
+
+var userAge = +prompt('Yoshingiz nechchida?')
+
+
+var userPlus = +prompt('Masalaning javobini toping 6 + 7')
+var userMinus = +prompt('Masalaning javobini toping 56 - 23')
+var userKupay = +prompt('Masalaning javobini toping 11 * 3')
+var userBol = +prompt('Masalaning javobini toping 33 / 12')
+var userQoldiq = +prompt('Masalaning javobini toping 48 % 4')
+
+var plus = 6 + 7
+var minus = 56 - 23
+var kopay = 11 * 3
+var bol = 33 / 12
+var qoldiq = 48 % 4
+
+
+console.info('6 + 7 =', plus, '(Sizning javob', userPlus, ')')
+console.info('56 - 23 =', plus, '(Sizning javob', userMinus, ')')
+console.info('11 * 3 =', plus, '(Sizning javob', userKupay, ')')
+console.info('33 / 12 =', plus, '(Sizning javob', userBol, ')')
+console.info('48 % 4 =', plus, '(Sizning javob', userQoldiq, ')')
